d43216a3cff0184bd15171bca737c0450ef23d50
Alex Petrov <alexp@coffeenco.de>
2014-06-09 23:14:22 +0200

    Add cheshire dep

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
+  :resource-paths ["resources" "cljs"]
   :dependencies [[org.clojure/clojure      "1.5.1"]
                  [enlive                   "1.1.5"]
                  [me.raynes/fs             "1.4.4"]
+                 [cheshire                 "5.3.1"]
                  [clojurewerkz/statistiker "0.1.0-SNAPSHOT"]])
