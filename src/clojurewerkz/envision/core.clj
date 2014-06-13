6c681ae04b7882e5c22162478f88f2655ede012d
Alex Petrov <alexp@coffeenco.de>
2014-06-13 23:55:27 +0200

    Add a couple of examples

--- src/clojurewerkz/envision/core.clj
+++ src/clojurewerkz/envision/core.clj
@@ -5,8 +5,10 @@
             [schema.core                        :as s]
             [clojurewerkz.envision.chart-config :as cfg]
 
-            [clojurewerkz.statistiker.histograms :as hist]
-            [clojurewerkz.statistiker.regression :as regression]
+            [clojurewerkz.statistiker.histograms   :as hist]
+            [clojurewerkz.statistiker.regression   :as regression]
+            [clojurewerkz.statistiker.distribution :as distribution]
+
             [clojure.java.browse :refer [browse-url]]))
 
 
@@ -34,52 +36,89 @@
 
 (defn histogram
   "Histogram accepts a vector of values"
-  [bins data]
+  [bins data & config-overrides]
   (let [hist (->> (hist/empirical-distribution bins data)
-                  (map (fn [[x y]] {:x x :y y})))]
-    (prepare-tmp-dir
-     [(cfg/make-chart-config
-       {:id            "histogram"
-        :x             "x"
-        :y             "y"
-        :series-type   "bar"
-        :data          hist})])))
+                  (map (fn [[x y]] {:x (format "%.3f" x) :y y})))]
+    (cfg/make-chart-config
+     (merge
+      {:id          "histogram"
+       :headline    "Histogram"
+       :x           "x"
+       :x-config    {:order-rule :x}
+       :y           "y"
+       :series-type "bar"
+       :data        hist}
+      (first config-overrides)))))
 
 (defn linear-regression
-  [data x-field y-field series]
+  [data x-field y-field series & config-overrides]
   (let [d             (map x-field data)
         [min-x max-x] [(apply min d) (apply max d)]]
-    (prepare-tmp-dir
-     [(cfg/make-chart-config
-       {:id                "bubble"
-        :x                 x-field
-        :x-type            :measure
-        :y                 y-field
-        :y-type            :measure
-        :x-config          {:order-rule "year"
-                            :override-min min-x}
-        :series-type       "bubble"
-        :data              data
-        :series            series
-        :additional-series [:linear-trend {:data (let [{:keys [intercept slope]} (regression/linear-regression
-                                                                                  data x-field y-field)]
+    (cfg/make-chart-config
+     (merge
+      {:id                "bubble"
+       :headline          "Linear Regression"
+       :x                 x-field
+       :x-type            :measure
+       :y                 y-field
+       :y-type            :measure
+       :x-config          {:order-rule "year"
+                           :override-min min-x}
+       :series-type       "bubble"
+       :data              data
+       :series            series
+       :additional-series [:linear-trend {:data (let [{:keys [intercept slope]} (regression/linear-regression
+                                                                                 data x-field y-field)]
 
-                                                   [{:cx min-x :cy (+ intercept (* slope min-x))},
-                                                    {:cx max-x :cy (+ intercept (* slope max-x))}])}]
-        })
-      ])))
+                                                  [{:cx min-x :cy (+ intercept (* slope min-x))},
+                                                   {:cx max-x :cy (+ intercept (* slope max-x))}])}]
+       }
+      (first config-overrides)))))
 
 
 (defn a []
-  (linear-regression
-   (flatten (for [i (range 0 20)]
-              [{:year (+ 2000 i)
-                :income (+ 10 i (rand-int 10))
-                :series "series-1"}
-               {:year (+ 2000 i)
-                :income (+ 10 i (rand-int 20))
-                :series "series-2"}]
-              ))
-   :year
-   :income
-   [:year :income :series]))
+  (prepare-tmp-dir
+   [(histogram 10 (take 100 (distribution/normal-distribution 5 10))
+               {:tick-format "s"})
+
+    (linear-regression
+     (flatten (for [i (range 0 20)]
+                [{:year (+ 2000 i)
+                  :income (+ 10 i (rand-int 10))
+                  :series "series-1"}
+                 {:year (+ 2000 i)
+                  :income (+ 10 i (rand-int 20))
+                  :series "series-2"}]
+                ))
+     :year
+     :income
+     [:year :income :series])
+    (cfg/make-chart-config
+     {:id            "line"
+      :headline      "Line Chart"
+      :x             "year"
+      :y             "income"
+      :x-config      {:order-rule "year"}
+      :series-type   "line"
+      :data          (flatten (for [i (range 0 20)]
+                                [{:year (+ 2000 i)
+                                  :income (+ 10 i (rand-int 10))
+                                  :series "series-1"}
+                                 {:year (+ 2000 i)
+                                  :income (+ 10 i (rand-int 20))
+                                  :series "series-2"}]
+                                ))
+      :series        "series"
+      :interpolation :cardinal
+      })
+    (cfg/make-chart-config
+     {:id            "area"
+      :headline      "Area Chart"
+      :x             "year"
+      :y             "income"
+      :x-config      {:order-rule "year"}
+      :series-type   "area"
+      :data          (into [] (for [i (range 0 20)] {:year (+ 2000 i) :income (+ 10 i (rand-int 10))}))
+      :interpolation :cardinal
+      })
+    ]))
