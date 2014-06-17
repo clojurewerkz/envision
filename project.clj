ea631c9426f9188bd22f24d5f80e61b20a18e06f
Alex Petrov <alexp@coffeenco.de>
2014-06-17 22:16:19 +0200

    Fix source path

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clj"]
+  :source-paths ["src"]
   :resource-paths ["resources" "cljs"]
   :dependencies [[org.clojure/clojure      "1.5.1"]
                  [enlive                   "1.1.5"]
