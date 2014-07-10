cb09aef19d9a29d00d69d066aae29ac4d91b29d6
Alex Petrov <alexp@coffeenco.de>
2014-07-10 19:28:18 +0200

    Downgrade clojurescript

--- cljs/project.clj
+++ cljs/project.clj
@@ -1,13 +1,14 @@
 (defproject clojurewerkz/envisioncljs "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojurescript "0.0-2197"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"
                   :exclusions [org.apache.ant/ant]]
                  [reagent "0.4.2"
                   :exclusions [org.clojure/clojurescript]]
-                 [cljs-ajax "0.2.2"]
-                 [clojurewerkz/balagan "1.0.0"]
-                 [prismatic/schema "0.2.2"]]
+                 ;; [cljs-ajax "0.2.2"]
+                 [clojurewerkz/balagan "1.0.0" :exclusions [org.clojure/clojure]]
+                 [prismatic/schema "0.2.4"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
 
   :cljsbuild {:builds [{:source-paths ["./src"]
                         :compiler {:output-to "../resources/assets/cljs/main.js"
